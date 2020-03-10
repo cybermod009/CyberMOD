649f305ecf5e11512216c47a3a1c936608551bc9
cybermod009 <62005072+cybermod009@users.noreply.github.com>
2020-03-10 06:29:54 +0000

    Update and rename cybermod.php to README.php

---
+++ README.php
@@ -0,0 +1,17 @@
+pkg update
+
+pkg upgrade
+
+pkg install php
+
+pkg install curl
+
+pkg install git
+
+git clone https://github.com/cybermod009/CyberMOD
+
+termux-setup-storage
+
+cd /sdcard/(file yg sudah di extrak name folder)
+
+php cybermod.php
